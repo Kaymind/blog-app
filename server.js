11d726b338b79ed9d216c71b8d49700cfaa2af75
Kaymind <lempelziv@gmail.com>
2019-09-05 21:14:25 +0700

    changes ui

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express    = require('express'),
     Blog       = require('./models/blog.model');
 
 //APP configurations
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.set('view engine','ejs');
@@ -17,16 +17,34 @@ app.set('view engine','ejs');
 // });
 
 //RESTful ROUTES
+
+
 app.get('/',function(req, res){
     res.redirect('/blogs');
 });
-
+//INDEX ROUTE
 app.get('/blogs',function(req, res){
     Blog.find({},function(err, blogs){
         if(err){
             console.log('ERROR!!!');
         }else {
             res.render('index', {blogs: blogs});
+        }
+    })
+})
+//NEW ROUTE
+app.get('/blogs/new',function(req, res){
+    res.render('new');
+})
+//CREATE ROUTE
+app.post('/blogs',function(req, res){
+    //create blog
+    Blog.create(req.body.blog,function(err, newBlog){
+        if(err){
+            res.render('new');
+        }else {
+            console.log(req.body.blog)
+            res.redirect('/blogs');
         }
     })
 })
